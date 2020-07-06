a7b5272a8e3de96a6f5db5aa34579a249b324051
buckket <felix@buckket.org>
2020-07-06 22:58:32 +0200

    Initial commit

---
+++ hk1/download_videos.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+youtube-dl -f 298+140 "https://www.youtube.com/watch?v=_xS0kcZ8dfk"
+youtube-dl -f 135+140 "https://www.youtube.com/watch?v=yR021OLSY2U"
+youtube-dl -f 22 "https://www.youtube.com/watch?v=esVZA_Vi2ko"
+
